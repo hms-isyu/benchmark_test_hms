cefc94d7e73019a0c20e0bee458a2133730fc4f5
isyu <isaac.yuki@hms-networks.com>
2026-08-03 20:44:19 +0200

    push porting layer to board specific folder
    Signed-off-by: isyu <isaac.yuki@hms-networks.com>

--- port/frdm_mcxn947/zephyr/benchmark_config.h
+++ port/frdm_mcxn947/zephyr/benchmark_config.h
@@ -2,7 +2,7 @@
 **    Copyright (C) 1999-2026 HMS Technology Center Ravensburg GmbH, all rights reserved
 ****************************************************************************************************
 **
-**        File: BENCHMARK_common.h
+**        File: benchmark_config.h
 **     Summary: ETX project specific configuration
 **              NXP MCXN947, ARM Cortex-M33 (FRDM-MCXN947, ETX benchmark)
 **
@@ -17,53 +17,17 @@
 **    include-files
 ***************************************************************************************************/
 
-#include "stdint.h"
-#include "stddef.h"
-
-#ifdef __CORTEX_M
-#include "core_cm33.h"
-#else
-#error "Unsupported core architecture. This benchmark is intended for specific architectures."
-#endif
+#include "stdbool.h"
+#include "cmsis_core.h"
 
 /***************************************************************************************************
 **    definitions
 ***************************************************************************************************/
 
-#define TEST_SIGNALING_EVNT_DURATION (100U) /* in ms */
+#define TASK_MUST_RETURN true
 
-__attribute__((always_inline)) static inline uint32_t BENCHMARK_get_counter_value(void)
-{
-#ifdef __CORTEX_M
-    return DWT->CYCCNT;
-#else
-    return 0U;
-#endif
-}
-
-__attribute__((always_inline)) static inline void BENCHMARK_reset_counter(void)
-{
-#ifdef __CORTEX_M
-    DWT->CYCCNT = 0U;
-#else
-    return;
-#endif
-}
+#define BENCHMARK_TIMER_FREQUENCY (SystemCoreClock) /* in MHz */
 
 /***************************************************************************************************
 **    prototypes
 ***************************************************************************************************/
-
-extern void BENCHMARK_hardware_init(void);
-
-extern void BENCHMARK_signal_measurement_start(void);
-
-extern void BENCHMARK_signal_measurement_stop(void);
-
-extern void BENCHMARK_signal_jitter_detected(uint32_t *array, size_t size);
-
-extern void BENCHMARK_assertQuietSystem(void);
-
-extern void BENCHMARK_assertNeededComponents(void);
-
-extern uint32_t BENCHMARK_MS_to_Ticks(uint32_t ms);
---
+++ port/frdm_mcxn947/zephyr/benchmark_zephyr.c
@@ -0,0 +1,110 @@
+/***************************************************************************************************
+**    Copyright (C) 1999-2026 HMS Technology Center Ravensburg GmbH, all rights reserved
+****************************************************************************************************
+**
+**        File: benchmark_etx.c
+**     Summary: ETX project specific configuration
+**              NXP MCXN947, ARM Cortex-M33 (FRDM-MCXN947, ETX benchmark)
+**
+**      Author: Isaac L. L. Yuki
+**
+****************************************************************************************************
+**    Template Version 4
+***************************************************************************************************/
+
+/***************************************************************************************************
+**    include-files
+***************************************************************************************************/
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+
+#include "benchmark_common.h"
+#include "benchmark_config.h"
+
+/***************************************************************************************************
+**    definitions
+***************************************************************************************************/
+
+/***************************************************************************************************
+**    prototypes
+***************************************************************************************************/
+
+uint32_t BENCHMARK_MS_to_Ticks(uint32_t ms);
+
+/***************************************************************************************************
+**    variables
+***************************************************************************************************/
+
+static uint32_t signalize_event_duration_ticks = BENCHMARK_MS_to_Ticks(TEST_SIGNALING_EVNT_DURATION);
+static bool     HW_initialized                 = false;
+
+static const struct gpio_dt_spec led  = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+
+#define LED_BLUE_TOGGLE() gpio_pin_toggle_dt(&led2)
+#define LED_RED_TOGGLE() gpio_pin_toggle_dt(&led)
+#define LED_GREEN_TOGGLE() gpio_pin_toggle_dt(&led1)
+
+/***************************************************************************************************
+**    code
+***************************************************************************************************/
+
+uint32_t BENCHMARK_MS_to_Ticks(uint32_t ms)
+{
+    return (ms * BENCHMARK_TIMER_FREQUENCY) / 1000U;
+}
+
+void BENCHMARK_hardware_init(void)
+{
+    /*done by board early init hook just check*/
+
+    assert(gpio_is_ready_dt(&led));
+    assert(gpio_is_ready_dt(&led1));
+    assert(gpio_is_ready_dt(&led2));
+    int ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+    assert(ret == 0);
+    ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_INACTIVE);
+    assert(ret == 0);
+    ret = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_INACTIVE);
+    assert(ret == 0);
+
+    HW_initialized = true;
+}
+
+void BENCHMARK_signal_measurement_start(void)
+{
+    assert(HW_initialized == true);
+    LED_BLUE_TOGGLE();
+    for (uint32_t i = 0; i < signalize_event_duration_ticks; i++)
+    {
+        __NOP();
+    }
+    LED_BLUE_TOGGLE();
+}
+
+void BENCHMARK_signal_measurement_stop(void)
+{
+    assert(HW_initialized == true);
+    LED_BLUE_TOGGLE();
+    for (uint32_t i = 0; i < signalize_event_duration_ticks; i++)
+    {
+        __NOP();
+    }
+    LED_BLUE_TOGGLE();
+}
+
+void BENCHMARK_signal_jitter_detected(uint32_t *array, size_t size)
+{
+    (void) array;
+    (void) size;
+    assert(HW_initialized == true);
+
+    LED_RED_TOGGLE();
+    for (uint32_t i = 0; i < signalize_event_duration_ticks; i++)
+    {
+        __NOP();
+    }
+    LED_RED_TOGGLE();
+}
