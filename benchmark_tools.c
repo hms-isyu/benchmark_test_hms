f3602936c6af2f5456c24f633bad39751968f51d
isyu <isaac.yuki@hms-networks.com>
2026-08-06 14:55:08 +0200

    improve check_hw_influence
    - max was not beeing set at r[0]
    - assert needed components
    
    Signed-off-by: isyu <isaac.yuki@hms-networks.com>

--- benchmark_tools.c
+++ benchmark_tools.c
@@ -123,13 +123,17 @@ void BENCHMARK_check_hw_influence(uint32_t loop_count)
     uint32_t        outlier_count = 0;
     uint32_t        result[2]     = {0U, 0U}; /* result[0] = min, result[1] = max */
     uint32_t       *data          = result;
+
     BENCHMARK_assertQuietSystem();
+    BENCHMARK_assertNeededComponents();
+
     for (uint32_t r = 0; r < 64U; r++)
     {
         raw[r] = BENCHMARK_measureEmptyLoop(loop_count);
         if (r == 0)
         {
             min = raw[r];
+            max = raw[r];
             continue;
         }
         else
